5a1c205376c9c1cb444c322a2a7145971196d0be
shenglongy <shenglongy@opera.com>
2022-10-17 15:31:01 +0800

    feat: add link on homepage

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from "react";
 
+import { Link } from "react-router-dom";
+
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <div className="p-4">
+      <Link className="text-blue-600 hover:text-blue-500" to={"/weektime"}>
+        Weektime
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
